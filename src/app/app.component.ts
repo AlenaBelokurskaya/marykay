469503a0fd9320df480bf21f8ea579e3716152e7
AlenaBelokurskaya <a.belokurskaya@gmail.com>
2017-09-06 16:48:07 +0300

    изменение структкры разброс по файлам

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Уход за лицом';
+  unitOfProductions = [{
+    "id": 1,
+    "name": "Очищающий гель Botanical Effects",
+    "description": "10/18",
+    "price": 27.00
+  },
+    {
+      "id": 2,
+      "name": "Тонизирующий скраб Botanical Effects",
+      "description": "08/19",
+      "price": 33.00
+    },
+    {
+      "id": 3,
+      "name": "Освежающий тоник Botanical Effects",
+      "description": "10/19",
+      "price": 27.00
+    },
+    {
+      "id": 4,
+      "name": "Увлажняющий гель Botanical Effects",
+      "description": "05/18",
+      "price": 33.00
+    }]
 }
--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,12 @@
-import { NgModule, Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'app-root',
-  template: `<h2>{{title}}</h2>
-  <table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>description</th>
-            <th>price</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr *ngFor="let unitOfProduction of unitOfProductions">
-            <td>{{unitOfProduction.id}}</td>
-            <td>{{unitOfProduction.name}}</td>
-            <td>{{unitOfProduction.description}}</td>
-            <td>{{unitOfProduction.price}}</td>
-        </tr>
-    </tbody>
-  </table>`
-})
-class AppComponent{
-  title = 'Уход за лицом';
-  unitOfProductions = [{
-    "id": 1,
-    "name": "Очищающий гель Botanical Effects",
-    "description": "10/18",
-    "price": 27.00
-  },
-  {
-    "id": 2,
-    "name": "Тонизирующий скраб Botanical Effects",
-    "description": "08/19",
-    "price": 33.00
-  },
-  {
-    "id": 3,
-      "name": "Освежающий тоник Botanical Effects",
-      "description": "10/19",
-      "price": 27.00
-  },
-  {
-    "id": 4,
-      "name": "Увлажняющий гель Botanical Effects",
-      "description": "05/18",
-      "price": 33.00
-  }]
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
 }
 
-@NgModule({
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule ],
-  bootstrap: [ AppComponent ]
-})
-class AppModule{}
-
-platformBrowserDynamic()
-    .bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule);
 
