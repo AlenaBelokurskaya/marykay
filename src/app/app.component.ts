82debc8639091b21eba03c9910b9e77ee91d9ba7
AlenaBelokurskaya <a.belokurskaya@gmail.com>
2017-09-07 12:51:03 +0300

    добавила вывод общего количества строк

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,37 @@ export class AppComponent {
     "id": 1,
     "name": "Очищающий гель Botanical Effects",
     "description": "10/18",
-    "price": 27.00
+    "price": 27.00,
+    "inStock": 5
   },
-    {
-      "id": 2,
-      "name": "Тонизирующий скраб Botanical Effects",
-      "description": "08/19",
-      "price": 33.00
-    },
-    {
-      "id": 3,
-      "name": "Освежающий тоник Botanical Effects",
-      "description": "10/19",
-      "price": 27.00
-    },
-    {
-      "id": 4,
-      "name": "Увлажняющий гель Botanical Effects",
-      "description": "05/18",
-      "price": 33.00
-    }]
+  {
+    "id": 2,
+    "name": "Тонизирующий скраб Botanical Effects",
+    "description": "08/19",
+    "price": 33.00,
+    "inStock": 2
+  },
+  {
+    "id": 3,
+    "name": "Освежающий тоник Botanical Effects",
+    "description": "10/19",
+    "price": 27.00,
+    "inStock": 1
+  },
+  {
+    "id": 4,
+    "name": "Увлажняющий гель Botanical Effects",
+    "description": "05/18",
+    "price": 33.00,
+    "inStock": 0
+  }];
+  totalUnitOfProductions() {
+    let sum = 0;
+
+    for (let unitOfProduction of this.unitOfProductions) {
+      sum += unitOfProduction.inStock;
+    }
+
+    return sum;
+  }
 }
