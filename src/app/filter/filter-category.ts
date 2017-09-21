68a0c25364311dcb0ec659f06aa74717041faf0e
AlenaBelokurskaya <a.belokurskaya@gmail.com>
2017-09-21 13:29:21 +0300

    изменила название фильтра

--- src/app/filter/filter-category.ts
+++ src/app/filter/filter-category.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform  } from '@angular/core';
 
 @Pipe({
-    name: 'myfilter'
+    name: 'categoryFilter'
 })
 export class MyFilterPipe implements PipeTransform {
     transform(unitOfProductions: any[], category: string): any {
